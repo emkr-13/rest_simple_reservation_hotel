adf558e1093ad88663d743a01abb52e328dba1f2
emkr-13 <krisna.rata13@gmail.com>
2025-06-12 10:11:34 +0700

    Update server import path and add room type seeder for bulk insertion of room types

---
+++ src/seeders/roomtypeSeeder.ts
@@ -0,0 +1,38 @@
+
+import { db } from "../config/db";
+import { roomTypes } from "../models/roomType​";
+async function seed() {
+  // Array data yang akan di-insert secara bulk
+  const roomTypesa = [
+    {
+      name: "Single Room",
+    },
+    {
+      name: "Double Room",
+    },
+    {
+      name: "Triple Room",
+    },
+    {
+      name: "Quadruple Room",
+    },
+    {
+        name: "VIP Room",
+    }
+  ];
+
+  // Lakukan bulk insert
+  await db.insert(roomTypes).values(roomTypesa);
+
+  console.log("Bulk insert room types completed");
+}
+
+seed()
+  .then(() => {
+    console.log("Seeder completed successfully");
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error("Error during seeding:", error);
+    process.exit(1);
+  });
